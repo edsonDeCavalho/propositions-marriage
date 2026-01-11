34ca0bffa27ec5f20b1fa7591936af3b344a4dbb
Edson Kennedy <edsonkennedy@Edsons-MacBook-Air.local>
2026-01-11 17:44:37 +0100

    presentatios

--- version2/vite.config.js
+++ version2/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8087,
+    port: 91,
     strictPort: true,
   },
 })
--- version3/vite.config.js
+++ version3/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8088,
+    port: 92,
     strictPort: true,
   },
 })
